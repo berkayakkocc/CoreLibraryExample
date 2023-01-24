4c631997b064af8e9b6a335a91d71eceade4a6fd
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-01-24 12:18:30 +0300

    ForMember in Profile

--- FluentValidationApp.Web/DTOs/CustomerDto.cs
+++ FluentValidationApp.Web/DTOs/CustomerDto.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
+        public string Isim { get; set; }
+        public string Eposta { get; set; }
+        public int Yas { get; set; }
     }
 }
